8976ba542c73818c23a7242cfcf31a052bdd7d24
Zach Kost-Smith <zachkostsmith@gmail.com>
2013-06-12 09:45:19 -0600

    Changed the interface so that you can have both lexical cleanup and dynamic cleanup.
    Cleaner implementation.

--- protected-bindings.lisp
+++ protected-bindings.lisp
@@ -7,34 +7,26 @@
 
 (defmacro with-protected-binding ((binding-var binding-form &rest binding-clean-up)
                                   &body body)
-  "This sets up a protected binding, a binding that has some clean-up code that
-will always be run when the binding expires.
-
-Usually, the forms specified at the end of the binding form will be placed in
-the clean-up clause of an unwind-protect.  If the clean-up forms starts with
-keyword :finalize, then the following code will be exectuted as if in a progn and
-the result will be set as a finalization thunk for the object (or produce an
-error if finalizers are not supported)."
-  (if (equal (first binding-clean-up) :finalize)
-      `(let ((,binding-var ,binding-form))
-         (tg:finalize ,binding-var ,@(rest binding-clean-up))
-         ,@body)
-      `(let ((,binding-var ,binding-form))
-         (unwind-protect
-              (progn ,@body)
-           ,@binding-clean-up))))
+  "Set up a single protected binding.  See WITH-PROTECTED-BINDINGS* for more
+info."
+  `(let ((,binding-var ,binding-form))
+     ,(when (equal (first binding-clean-up) :finalize)
+        (pop binding-clean-up)
+        `(tg:finalize ,binding-var ,(pop binding-clean-up)))
+     (unwind-protect
+          (progn ,@body)
+       ,@binding-clean-up)))
 
 ;; @This is particularly useful as this is actually annoying to do correctly.
 
 (defmacro with-protected-bindings* (bindings &body body)
-  "This sets up a protected bindings, a bindings that has some clean-up code
-that will always be run when the binding expires.
+  "This sets up protected bindings, a bindings that has some clean-up code that
+will always be run when the binding expires.  A binding is of the form:
 
-Usually, the forms specified at the end of the binding forms will be placed in
-the clean-up clause of an unwind-protect.  If the clean-up forms starts with
-keyword :finalize, then the following code will be exectuted as if in a progn
-and the result will be set as a finalization thunk for the object (or produce an
-error if finalizers are not supported)."
+ (sym value [:finalize evaluates-to-finalize-thunk] . clean-up-forms)
+
+CLEAN-UP-FORMS will be guaranteed to run via unwind-protect.  If there is
+a :FINALIZE value, the finalizer is instantiated."
   (if bindings
       `(with-protected-binding ,(first bindings)
          (with-protected-bindings* ,(rest bindings)
